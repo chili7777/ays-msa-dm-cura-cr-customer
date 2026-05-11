f98c7b22d71166138a2cb559b0c3d7fc85036f94
hanmi <hanmilton.777@gmail.com>
2026-05-10 22:07:33 -0500

    chore(customer): initialize empty spring boot project

---
+++ src/main/java/com/pichincha/dm/cura/customer/AysMsaDmCuraCrCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.pichincha.dm.cura.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AysMsaDmCuraCrCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AysMsaDmCuraCrCustomerApplication.class, args);
+	}
+
+}
